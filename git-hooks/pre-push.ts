c3457b9a28d8e0529abab1bc5209469c627cd7be
ddonovan <info@promatrixinc.com>
2021-01-09 08:22:18 -0500

    ...

--- git-hooks/pre-push.ts
+++ git-hooks/pre-push.ts
@@ -4,8 +4,7 @@ export class PrePush {
     
     constructor() {
         try {
-            
-            const noop = new TaskNpmPublish(false, 'ngx-motion', 'npm-v9', '..\\..\\NgResources\\ngx-motion', 'library', 'projects\\ng2-express\\dist', '..\\..\\Angular.Fire.Studio.11.00\\studio.app', 'package-ng2-express');
+            const noop = new TaskNpmPublish(false, 'ngx-motion', 'npm', '..\\..\\NgResources\\ngx-motion', 'library', 'projects\\ng2-express\\dist', '..\\..\\Angular.Fire.Studio.11.00\\studio.app', 'package-ng2-express');
             // we can only see the console.log is the process.exit(1);
             // process.exit(1);
         } catch (e) {
