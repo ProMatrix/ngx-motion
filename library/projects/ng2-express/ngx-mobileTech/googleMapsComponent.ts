ba059bbf843609c4c6115d042e4ab82c60236aeb
ddonovan <info@promatrixinc.com>
2021-05-06 03:23:06 -0400

    added debugger;

--- library/projects/ng2-express/ngx-mobileTech/googleMapsComponent.ts
+++ library/projects/ng2-express/ngx-mobileTech/googleMapsComponent.ts
@@ -27,6 +27,7 @@ export class GoogleMapsComponent {
   @Output() visibleChange = new EventEmitter<boolean>();
 
   constructor(private readonly cd: ChangeDetectorRef, private readonly ngZone: NgZone) {
+    debugger;
   }
 
   public initialize() {
